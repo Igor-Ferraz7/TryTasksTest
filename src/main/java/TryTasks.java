876fb24c37760423b06eaf363f9c72c6cad785f4
Igor Ferraz <igorsousaferrazaraga2@gmail.com>
2025-05-28 19:09:55 -0300

    Att project

---
+++ src/main/java/TryTasks.java
@@ -0,0 +1,11 @@
+import com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme;
+import interfaces.*;
+
+public class TryTasks {
+    public static void main(String[] args) {
+        FlatOneDarkIJTheme.setup(); 
+
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
